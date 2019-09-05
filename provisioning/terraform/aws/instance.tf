955b1eb76c4450eecbbee0876a377d34b581b702
Rafael G. de Matos <rafael.cgid@gmail.com>
2019-09-05 00:09:31 -0300

    upgrade

--- provisioning/terraform/aws/instance.tf
+++ provisioning/terraform/aws/instance.tf
@@ -7,5 +7,31 @@ provider "aws" {
 resource "aws_instance" "default" {
   ami = "ami-0378588b4ae11ec24"
   instance_type = "t2.micro"
+  key_name= "${aws_key_pair.default.key_name}"
+  security_groups = ["${aws_security_group.default.name}"]
 }
 
+resource "aws_key_pair" "default" {
+  key_name = "my-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_security_group" "default" {
+  name = "allow_ssh"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0 
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "output_public_dns" {
+  value = "${aws_instance.default.public_dns}"
+}
