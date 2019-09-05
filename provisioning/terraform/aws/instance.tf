114fc9a739589dcfce35799a0937784820d30f1e
Rafael G. de Matos <rafael.cgid@gmail.com>
2019-09-05 00:37:54 -0300

    new file and ident instance.tf

--- provisioning/terraform/aws/instance.tf
+++ provisioning/terraform/aws/instance.tf
@@ -1,33 +1,33 @@
 provider "aws" {
-  region    = "us-east-1"
-  shared_credentials_file   = "~/.aws_credentials"
-  profile = "suporte"
+  region                  = "us-east-1"
+  shared_credentials_file = "~/.aws_credentials"
+  profile                 = "suporte"
 }
 
 resource "aws_instance" "default" {
-  ami = "ami-0378588b4ae11ec24"
-  instance_type = "t2.micro"
-  key_name= "${aws_key_pair.default.key_name}"
+  ami             = "ami-0378588b4ae11ec24"
+  instance_type   = "t2.micro"
+  key_name        = "${aws_key_pair.default.key_name}"
   security_groups = ["${aws_security_group.default.name}"]
 }
 
 resource "aws_key_pair" "default" {
-  key_name = "my-key"
+  key_name   = "my-key"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
 resource "aws_security_group" "default" {
   name = "allow_ssh"
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0 
-    protocol = -1
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
