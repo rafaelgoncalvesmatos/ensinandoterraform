91b8dc666c05453599c93434d8e34e0c52474c98
Rafael G. de Matos <rafael.cgid@gmail.com>
2019-09-11 00:53:40 -0300

    Creating lab 2

---
+++ provisioning/terraform/aws/lab-02/instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "default" {
+  ami             = "${var.ami_type}"
+  instance_type   = "${var.instance_type}"
+  key_name        = "${aws_key_pair.default.key_name}"
+  security_groups = ["${aws_security_group.default.name}"]
+  count           = "${var.quantidade}"
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = "my-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
